f1114b0aafceac8fbe5338239643d293505ec1b4
NhuNhung <Ngnhung4685@gmail.com>
2023-12-10 21:20:17 +0700

    test

--- ManageFlightApp/controller.py
+++ ManageFlightApp/controller.py
@@ -1,4 +1,4 @@
-from ManageFlightApp import app
+from ManageFlightApp import app, login, utils
 from flask import render_template
 
 
@@ -7,5 +7,11 @@ def index():
     return render_template("index.html")
 
 
+@login.user_loader
+def load_user(user_id):
+    return utils.get_user_by_id(user_id)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ ManageFlightApp/utils.py
@@ -0,0 +1,5 @@
+from ManageFlightApp.models import User
+
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
