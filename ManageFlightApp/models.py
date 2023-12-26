c5a28ccf1eeb79a7bcdd21d51d9b2ea2f823d3fa
NhuNhung <Ngnhung4685@gmail.com>
2023-12-26 23:47:54 +0700

    n

--- ManageFlightApp/models.py
+++ ManageFlightApp/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, DATETIME
+from datetime import datetime
 from ManageFlightApp import db, app
 from flask_login import UserMixin
 import enum
@@ -18,6 +19,66 @@ class User(db.Model, UserMixin):
     avatar = Column(String(100),
                     default='https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg')
     user_role = Column(Enum(UserRoleEnum), default=UserRoleEnum.USER)
+
+
+class Sticket(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    issuancedate = Column(DATETIME, default=datetime.now())
+
+
+class TicketClass(db.Model):
+    id = Column(Integer, primary_key=True)
+
+
+class bill(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    issuancedate = Column(DATETIME, default=datetime.now())
+
+
+class TuyenBay(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    diemxuatphat = Column(String)
+    diemden = Column(String)
+    tgiandichuyen = Column(String)
+
+class SanBay(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tensanbay = Column(String)
+
+
+class LichBay(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    thoigiandi = Column(String)
+    thoigianden = Column(String)
+
+
+class ChuyenBay(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    trangthai = Column(String)
+
+
+class VeMayBay(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ngayxuatve = Column(DATETIME)
+
+
+class HangVe(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+
+class GheNgoi(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+
+class MayBay(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tenmaybay = Column(String)
+
+
+class HangMayBay(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tenhang = Column(String)
+
 
 
 if __name__ == '__main__':
