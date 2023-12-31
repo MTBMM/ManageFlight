0db9f8ba117f7f624d7aefed073b9aa222746c99
MTBMM <kiennguyentrung408gmail.com>
2023-12-31 07:59:05 +0700

    kien

--- ManageFlightApp/models.py
+++ ManageFlightApp/models.py
@@ -132,8 +132,8 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        import hashlib
-
+        # import hashlib
+        #
         # u1 = Employee(name='Admin', username='admin',
         #               password=str(hashlib.md5('123456'.encode('utf-8')).hexdigest()), salary=50000000,
         #               user_role=UserRoleEnum.ADMIN)
@@ -161,13 +161,13 @@ if __name__ == '__main__':
         #
         # db.session.add_all([u1, u2, u3, u4, u5, u6, u7, u8, u9, u10])
         # db.session.commit()
-
+        #
         # k1 = TicketClass(name='Hang Pho Thong', quantity=15)
         # k2 = TicketClass(name='Hang Thuong Gia', quantity=10)
         #
         # db.session.add_all([k1, k2])
         # db.session.commit()
-
+        #
         # A1 = Airport(name='TP.HCM', location='Bình Thạnh, Thành phố Hồ Chí Minh')
         # A2 = Airport(name='Hà Nội', location='Linh Xuân, Hà Nội')
         # A3 = Airport(name='Hải Phòng', location='Đóng Đa, Hải Phòng')
@@ -182,7 +182,7 @@ if __name__ == '__main__':
         #
         # db.session.add_all([A1, A2, A3, A4, A5, A6, A7, A8, A9, A10])
         # db.session.commit()
-
+        #
         # r1 = Route(name='TP.HCM - HÀ NỘI', distance=1190, number_of_airport=2, arrival_id=2, departure_id=1)
         # r2 = Route(name='TP.HCM - Đà Nẵng', distance=590, number_of_airport=0, arrival_id=4, departure_id=1)
         # r3 = Route(name='Đà Nẵng - Hải Phòng', distance=200, number_of_airport=0, arrival_id=3, departure_id=4)
@@ -197,7 +197,7 @@ if __name__ == '__main__':
         #
         # db.session.add_all([r1, r2, r3, r4, r5, r6, r7, r8, r9, r10])
         # db.session.commit()
-
+        #
         # St1 = Stop(route_id=1, airport_id=4, order=1, arrival_time='2023-12-31 11:00:00', time_delay_max=30,
         #            time_delay_min=20)
         # St2 = Stop(route_id=1, airport_id=3, order=2, arrival_time='2023-12-31 14:00:00', time_delay_max=30,
@@ -217,10 +217,10 @@ if __name__ == '__main__':
         #
         # db.session.add_all([St4, St5, St6, St7])
         # db.session.commit()
-
+        #
         # db.session.add_all([St1, St2, St3, St, St4, St5, St6, St7])
         # db.session.commit()
-
+        #
         # p1 = Plane(name='Máy bay 1')
         # p2 = Plane(name='Máy bay 2')
         # p3 = Plane(name='Máy bay 3')
@@ -234,7 +234,7 @@ if __name__ == '__main__':
         #
         # db.session.add_all([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10])
         # db.session.commit()
-
+        #
         # S1 = Seat(number_seat='A01', plane_id=1, status=True)
         # S2 = Seat(number_seat='A02', plane_id=1, status=True)
         # S3 = Seat(number_seat='A03', plane_id=1, status=True)
@@ -265,13 +265,13 @@ if __name__ == '__main__':
         #     [S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15, S16, S17, S18, S19, S20, S21, S22, S23,
         #      S24, S25])
         # db.session.commit()
-
+        #
         # f1 = Flight(route_id=1, departure_time='2023-12-31 9:00:00', arrival_time='2023-12-31 17:00:00')
         # f2 = Flight(route_id=8, departure_time='2023-1-31 13:30:00', arrival_time='2024-02-1 01:00:00')
         #
         # db.session.add_all([f1, f2])
         # db.session.commit()
-
+        #
         # Rc1 = Receipt(created_date='2023-12-30 09:45:00', user_id=1, ticket_class_id=1, flight_id=1, quantity=3,
         #               unit_price=3600000)
         # Rc2 = Receipt(created_date='2023-12-30 19:15:00', user_id=2, ticket_class_id=2, flight_id=1, quantity=2,
@@ -279,55 +279,55 @@ if __name__ == '__main__':
         # Rc3 = Receipt(created_date='2023-12-22 21:10:00', user_id=3, ticket_class_id=2, flight_id=1, quantity=2,
         #               unit_price=3000000)
         #
-        Rc1 = Receipt(created_date='2023-12-30 09:45:00', user_id=1, employee_id=2, ticket_class_id=1, flight_id=1,
-                      quantity=3,
-                      unit_price=3600000)
-        Rc2 = Receipt(created_date='2023-12-30 19:15:00', user_id=2, employee_id=2, ticket_class_id=2, flight_id=1,
-                      quantity=2,
-                      unit_price=3000000)
-        Rc3 = Receipt(created_date='2023-12-22 21:10:00', user_id=3, employee_id=2, ticket_class_id=2, flight_id=1,
-                      quantity=2,
-                      unit_price=3000000)
-
+        # Rc1 = Receipt(created_date='2023-12-30 09:45:00', user_id=1, employee_id=2, ticket_class_id=1, flight_id=1,
+        #               quantity=3,
+        #               unit_price=3600000)
+        # Rc2 = Receipt(created_date='2023-12-30 19:15:00', user_id=2, employee_id=2, ticket_class_id=2, flight_id=1,
+        #               quantity=2,
+        #               unit_price=3000000)
+        # Rc3 = Receipt(created_date='2023-12-22 21:10:00', user_id=3, employee_id=2, ticket_class_id=2, flight_id=1,
+        #               quantity=2,
+        #               unit_price=3000000)
+        #
         # db.session.add_all([Rc1, Rc2, Rc3])
         # db.session.commit()
-
-        Tp1 = TicketPrice(price=1200000, ticket_class_id=1, flight_id=1)
-        Tp2 = TicketPrice(price=1500000, ticket_class_id=2, flight_id=1)
-
-        Tp3 = TicketPrice(price=1700000, ticket_class_id=1, flight_id=2)
-        Tp4 = TicketPrice(price=2200000, ticket_class_id=2, flight_id=2)
-        db.session.add_all([Tp1, Tp2, Tp3, Tp4])
-        db.session.commit()
-
+        #
+        # Tp1 = TicketPrice(price=1200000, ticket_class_id=1, flight_id=1)
+        # Tp2 = TicketPrice(price=1500000, ticket_class_id=2, flight_id=1)
+        #
+        # Tp3 = TicketPrice(price=1700000, ticket_class_id=1, flight_id=2)
+        # Tp4 = TicketPrice(price=2200000, ticket_class_id=2, flight_id=2)
         # db.session.add_all([Tp1, Tp2, Tp3, Tp4])
         # db.session.commit()
-
-        T1 = Ticket(purchase_time='2023-12-31 02:00:00', seat_id=1, fight_id=1, ticket_class_id=1)
-        T2 = Ticket(purchase_time='2023-12-31 02:00:00', seat_id=2, fight_id=1, ticket_class_id=1)
-        T3 = Ticket(purchase_time='2023-12-31 02:00:00', seat_id=3, fight_id=1, ticket_class_id=1)
-        T4 = Ticket(purchase_time='2023-12-31 01:00:00', seat_id=16, fight_id=1, ticket_class_id=1)
-        T5 = Ticket(purchase_time='2023-12-31 01:00:00', seat_id=17, fight_id=1, ticket_class_id=1)
-        T6 = Ticket(purchase_time='2023-12-31 03:00:00', seat_id=18, fight_id=1, ticket_class_id=1)
-        T7 = Ticket(purchase_time='2023-12-31 03:0:00', seat_id=19, fight_id=1, ticket_class_id=1)
-
-        db.session.add_all([T1, T2, T3, T4, T5, T6, T7])
-        db.session.commit()
-
-        rcd1 = ReceiptDetail(ticket_id=1, receipt_id=1)
-        rcd2 = ReceiptDetail(ticket_id=2, receipt_id=1)
-        rcd3 = ReceiptDetail(ticket_id=3, receipt_id=1)
-        rcd4 = ReceiptDetail(ticket_id=4, receipt_id=2)
-        rcd5 = ReceiptDetail(ticket_id=5, receipt_id=2)
-        rcd6 = ReceiptDetail(ticket_id=6, receipt_id=3)
-        rcd7 = ReceiptDetail(ticket_id=7, receipt_id=3)
-
-        db.session.add_all([rcd1, rcd2, rcd3, rcd4, rcd5, rcd6, rcd7])
-        db.session.commit()
-
-        Sc1 = Schedules(plane_id=1, flight_id=1)
-        Sc2 = Schedules(plane_id=2, flight_id=1)
-        Sc3 = Schedules(plane_id=3, flight_id=1)
-
-        db.session.add_all([Sc1, Sc2, Sc3])
-        db.session.commit()
+        #
+        # db.session.add_all([Tp1, Tp2, Tp3, Tp4])
+        # db.session.commit()
+        #
+        # T1 = Ticket(purchase_time='2023-12-31 02:00:00', seat_id=1, fight_id=1, ticket_class_id=1)
+        # T2 = Ticket(purchase_time='2023-12-31 02:00:00', seat_id=2, fight_id=1, ticket_class_id=1)
+        # T3 = Ticket(purchase_time='2023-12-31 02:00:00', seat_id=3, fight_id=1, ticket_class_id=1)
+        # T4 = Ticket(purchase_time='2023-12-31 01:00:00', seat_id=16, fight_id=1, ticket_class_id=1)
+        # T5 = Ticket(purchase_time='2023-12-31 01:00:00', seat_id=17, fight_id=1, ticket_class_id=1)
+        # T6 = Ticket(purchase_time='2023-12-31 03:00:00', seat_id=18, fight_id=1, ticket_class_id=1)
+        # T7 = Ticket(purchase_time='2023-12-31 03:0:00', seat_id=19, fight_id=1, ticket_class_id=1)
+        #
+        # db.session.add_all([T1, T2, T3, T4, T5, T6, T7])
+        # db.session.commit()
+        #
+        # rcd1 = ReceiptDetail(ticket_id=1, receipt_id=1)
+        # rcd2 = ReceiptDetail(ticket_id=2, receipt_id=1)
+        # rcd3 = ReceiptDetail(ticket_id=3, receipt_id=1)
+        # rcd4 = ReceiptDetail(ticket_id=4, receipt_id=2)
+        # rcd5 = ReceiptDetail(ticket_id=5, receipt_id=2)
+        # rcd6 = ReceiptDetail(ticket_id=6, receipt_id=3)
+        # rcd7 = ReceiptDetail(ticket_id=7, receipt_id=3)
+        #
+        # db.session.add_all([rcd1, rcd2, rcd3, rcd4, rcd5, rcd6, rcd7])
+        # db.session.commit()
+        #
+        # Sc1 = Schedules(plane_id=1, flight_id=1)
+        # Sc2 = Schedules(plane_id=2, flight_id=1)
+        # Sc3 = Schedules(plane_id=3, flight_id=1)
+        #
+        # db.session.add_all([Sc1, Sc2, Sc3])
+        # db.session.commit()
